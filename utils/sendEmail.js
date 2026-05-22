d944973aa587fd7d8d8328e3dbb759e75e2b6df1
DevZeroSh <m.shahrour.work@gmail.com>
2026-05-22 14:47:21 +0300

    fixed Import

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,19 +2,15 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (options) => {
-  try {
-    const msg = {
-      to: options.email,
-      from: "SmartERP <no-reply@smartinb.com>",
-      subject: options.subject,
-      text: options.message,
-    };
+  const msg = {
+    to: options.email,
+    from: "SmartERP <no-reply@smartinb.com>",
+    subject: options.subject,
+    text: options.message,
+  };
 
-    await sgMail.send(msg);
-    console.log("Email sent 🐘");
-  } catch (err) {
-    console.error("Email fail 💀", err);
-  }
+  await sgMail.send(msg);
+  console.log("Email sent 🐘");
 };
 
 module.exports = sendEmail;
